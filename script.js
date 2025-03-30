ec4e3f24a3a899cf1801ad6a2c9fafd9b99c9db5
Durga-velu <durgadevi134babu@gmail.com>
2025-03-30 16:56:35 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,52 @@
 //your JS code here. If required.
+
+
+
+let buttons=document.querySelectorAll(".btn")
+let stop=document.querySelector(".stop")
+let soundAudio=null
+
+buttons.forEach((btn)=>{
+
+    btn.addEventListener("click",()=>{
+
+        if(soundAudio){
+     soundAudio.pause()
+     soundAudio.currentTime = 0;
+
+        }
+        soundAudio=new Audio(btn.getAttribute("data-sound"))
+
+        soundAudio.play()
+
+    })
+
+  
+})
+
+
+stop.addEventListener("click",()=>{
+
+if(soundAudio){
+    soundAudio.pause();
+    soundAudio.currentTime=0;
+}
+
+
+})
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
