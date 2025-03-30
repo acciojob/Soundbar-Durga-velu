aef3f4cd192cf830cc4bb4e89d42176e68d15fcd
Durga-velu <durgadevi134babu@gmail.com>
2025-03-30 20:13:53 +0530

    created a submission

--- script.js
+++ script.js
@@ -2,36 +2,28 @@
 
 
 
+// Select buttons and stop button
+let buttons = document.querySelectorAll(".btn");
+let stopButton = document.querySelector(".stop");
+let soundAudio = new Audio(); // Create a single audio element
 
-let buttons=document.querySelectorAll(".btn")
-let stop=document.querySelector(".stop")
-let soundAudio=null
-
-buttons.forEach((btn)=>{
-
-    btn.addEventListener("click",()=>{
-
-        if(soundAudio){
-     soundAudio.pause()
-     soundAudio.currentTime = 0;
-
-        }
-           let soundPath = btn.getAttribute("data-sound");
-        soundAudio = new Audio(soundPath);
-        soundAudio.play();
-
-    })
-
-  
-})
-
-
-stop.addEventListener("click",()=>{
-
-if(soundAudio){
-    soundAudio.pause();
-    soundAudio.currentTime=0;
+// Function to play sound
+function soundPlay(event) {
+    let soundPath = event.target.getAttribute("data-sound");
+    soundAudio.src = soundPath; // Set the source
+    soundAudio.play(); // Play audio
 }
 
+// Function to stop sound
+function soundStop() {
+    soundAudio.pause();
+    soundAudio.currentTime = 0;
+}
 
-})
+// Add click event to each button
+buttons.forEach((button) => {
+    button.addEventListener("click", soundPlay);
+});
+
+// Add click event to stop button
+stopButton.addEventListener("click", soundStop);
